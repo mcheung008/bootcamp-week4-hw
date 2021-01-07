69b7556e66f03e406bfe22977652b0d729d9e83b
mcheung008 <mcheung008@gmail.com>
2021-01-06 23:35:56 -0800

    redo

--- script.js
+++ script.js
@@ -1,80 +1,104 @@
-const startButton = document.getElementById("start-quiz-button")
-const questionContainerEl = document.getElementById ("question-box")
-const questionEl = document.getElementById("question")
-const choicesButtonsEl = document.getElementById("choices-buttons")
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
 let shuffledQuestions, currentQuestionIndex
-startButton.addEventListener("click" , startQuiz)
 
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-// const timerEl = document.getElementById("timer");
-// const secondsLeft = 180;
-  
-
-
-function startQuiz() {
-    
-    
-    startButton.classList.add("hide")
-    shuffledQuestions= questions.sort(() => Math.random() - .5)
-    questionContainerEl.classList.remove("hide")
-    currentQuestionIndex = 0
-    nextQuestion()
-
-
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-
-function nextQuestion() {
-
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-
-
-
-
-
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionEl.innerText = question.question
-    question.choices.forEach(choices => {
-        const button = document.createElement("button")
-        button.innerText = choices.text
-        button.classList.add("btn-btn-info")
-       
-        button.addEventListener("click" , selectChoices)
-        choicesButtonsEl.appendChild(button)
-    })
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
-
-
-
-
-
-function selectChoices(e) {
-
-
-
-
-
-
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
 
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
 
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
 
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
 
-
-// startBtn.addEventListener("click" , startTimer);
-
-// function startTimer() {
-//     var timerInterval = setInterval(function() {
-//         secondsLeft--;
-//         timerEl.textContent = "Time: " + secondsLeft;
-
-//         if(secondsLeft === 0) {
-//             alert("Times up!");
-//             clearInterval(timerInterval);
-//         }
-//     },1000)
-// }
-// startTimer();
+const questions = [
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  },
+    
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
+]
